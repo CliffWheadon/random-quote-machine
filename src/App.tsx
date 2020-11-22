5cdd5a49d132b8675a086e66ed0cb72a162c32a4
Cliff Wheadon <cliff.wheadon@gmail.com>
2020-11-22 02:23:36 -0400

    Centering the quotebox

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import './App.css';
 import RandomQuoteBox from './RandomQuoteBox';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
 
 function App() {
   return (
-    <Container>
-        <Row className={'my-auto mx-auto'}><RandomQuoteBox /></Row>
+    <Container className={"QuoteBoxContainer"}>
+        <Row className={"my-auto"}>
+            <Col/>
+            <Col xs={8}><RandomQuoteBox /></Col>
+            <Col/>
+        </Row>
     </Container>
   );
 }
--- src/RandomQuoteBox.tsx
+++ src/RandomQuoteBox.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
+import './RandomQuoteBox.css'
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons/faTwitter';
+import { faQuoteLeft} from '@fortawesome/free-solid-svg-icons/faQuoteLeft';
 
 /**
  * Renders a box with a random quote in it. The quote can be changed by clicking the button.
@@ -15,8 +17,15 @@ class RandomQuoteBox extends React.Component {
         return (
             <Jumbotron id={'quote-box'} className={'quote-box'}>
                 <Container>
-                    <Row><Col><p id={'text'} className={'text'}>Technically, this is a quote.</p></Col></Row>
-                    <Row><Col><p id={'author'} className={'author'}>- Clifford Wheadon</p></Col></Row>
+                    <Row>
+                        <Col xs={1}>
+                            <FontAwesomeIcon icon={faQuoteLeft} />
+                        </Col>
+                        <Col>
+                            <p id={'text'} className={'text'}>Technically, this is a quote.</p>
+                        </Col>
+                    </Row>
+                    <Row className={'authorRow'}><p id={'author'} className={'author'}>- Clifford Wheadon</p></Row>
                     <Row>
                         <Col>
                             <Button id={'tweet-quote'} href={'www.google.com'}>
